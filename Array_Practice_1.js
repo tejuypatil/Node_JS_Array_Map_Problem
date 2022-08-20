70755b19eb51028c00fbdfc8ab2a273628c04cd9
tejuypatil <tejuypatil@gmail.com>
2022-08-20 09:46:27 +0530

    Extended the first Program

--- Array_Practice_1.js
+++ Array_Practice_1.js
@@ -14,6 +14,13 @@
 
    console.log(arr);
 
+/*
+1. Write a program in the following steps
+    a. Generates 10 Random 3 Digit number.
+    b. Store this random numbers into a array.
+    c. Then find the 2nd largest and the 2nd smallest element without sorting the array.
+ */
+
    let smallest = 1000;
    let second_smallest = 1000;
    let largest = 0;
@@ -49,3 +56,17 @@
    console.log('second_smallest '+second_smallest)
    console.log('largest '+largest)
    console.log('second_largest '+second_largest)
+
+
+/**
+ * Extend the above program to sort the array and then find the 
+ * 2nd largest and the 2nd smallest element.
+ */
+
+let sortedArr=arr.sort();
+console.log(sortedArr);
+
+console.log('smallest '+sortedArr[0])
+console.log('second_smallest '+sortedArr[1])
+console.log('largest '+sortedArr[sortedArr.length-1])
+console.log('second_largest '+sortedArr[sortedArr.length-2])
